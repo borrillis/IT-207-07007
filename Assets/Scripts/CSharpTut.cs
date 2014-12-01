9f5db7f54d84394a968416a92ea93a0cd64865b1
unknown <m.cummings1@ACC-GAME-TS.lab.snhu.edu>
2014-12-01 13:21:58 -0500

    C# Tutorial

---
+++ Assets/Scripts/CSharpTut.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CSharpTut : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Variables myClass;
+        myClass = new Variables();
+
+        myClass.Test();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+public class Variables
+{
+    public void Test()
+    {
+        int x = 100;
+        int y;
+        y = 150;
+
+        int z;
+
+        z = x + y;
+
+        Debug.Log( String.Format( "{0}", z ) );
+
+        string Name = "Joe";
+        string Surname = "Schmoe";
+
+        Debug.Log(Name + " " + Surname);
+        Debug.Log(String.Concat( Name, " ", Surname));
+
+    }
+}
